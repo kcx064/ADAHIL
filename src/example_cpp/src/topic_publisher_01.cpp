82b6612f7f9c99e17c487675d72d2f547049d881
kcx064 <kcx064@163.com>
2024-05-28 23:30:00 +0800

    add custom msg

--- src/example_cpp/src/topic_publisher_01.cpp
+++ src/example_cpp/src/topic_publisher_01.cpp
@@ -1,15 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include <sys/time.h>
 #include "posix/spi.h"
+#include <string.h>
+
 
 extern "C"{
 	#include "fadd.h"
-	// #include "mchilmodel.h"
 	#include "libsimodel.h"
 }
 
 
-#define spi0_device		"/dev/spidev0.0"
+#define spi0_device		"/dev/spidev1.0"
 
 class TopicPublisher01 : public rclcpp::Node, public SPI
 {
@@ -21,7 +23,7 @@ public:
 		
 		//初始化SPI，并进行测试
 		//initial spi0
-		int ret = SPI::init(spi0_device, SPIDEV_MODE3, 10000000);
+		int ret = SPI::init(spi0_device, SPIDEV_MODE3, 18000000);//SPIDEV_MODE3
 		if (ret != PX4_OK) {
 			printf("SPI::init failed (%i)", ret);
 		}
@@ -51,52 +53,81 @@ public:
 		reg_data = RegisterRead(0x05);
 		printf("data read is: %d\n", reg_data);
 
-		fadd_initialize();
 		libsimodel_initialize();
 
 		// 创建发布者
         command_publisher_ = this->create_publisher<std_msgs::msg::String>("command", 10);
-		// 创建定时器，500ms为周期，定时发布
+		// 创建定时器，xxxms为周期，定时发布
 		//milliseconds表示毫秒  microseconds表示微妙
-        timer_ = this->create_wall_timer(std::chrono::microseconds(500), std::bind(&TopicPublisher01::timer_callback, this));
+        timer_ = this->create_wall_timer(std::chrono::microseconds(125), std::bind(&TopicPublisher01::timer_callback, this));
     }
 
 private:
+	struct timespec tBeginTime, tEndTime;
+	struct timespec tBeginTime_spi, tEndTime_spi;
 	// Transfer data
-	struct TransferBuffer {
+	struct WriteBuffer {
 		uint8_t cmd{0x00 | 0x00};
-		uint8_t Accel_data[6] {0,};
-		uint8_t Temp_data[2] {0,};
 		uint8_t Gyro_data[6] {0,};
+		uint8_t Temp_data[2] {0,};
+		uint8_t Accel_data[6] {0,};
+		uint8_t Mag_data[6] {0,};
+		uint8_t Pressure_data[6] {0,};
+	};
+	struct ReadBuffer {
+		uint8_t cmd{0x00 | 0x80};
+		uint8_t Gyro_data[6] {0,};
+		uint8_t Temp_data[2] {0,};
+		uint8_t Accel_data[6] {0,};
+		uint8_t Mag_data[6] {0,};
+		uint8_t Pressure_data[6] {0,};
 	};
 	void timer_callback()
     {
-		TransferBuffer buffer{};
+		//得到当前时间，然后与之前的时间计算插值
+		clock_gettime(CLOCK_MONOTONIC, &tEndTime);
+		double fCostTime = (tEndTime.tv_sec-tBeginTime.tv_sec)*1000000 + (double)(tEndTime.tv_nsec-tBeginTime.tv_nsec)/1000;
+		memcpy(&tBeginTime,&tEndTime,sizeof(tBeginTime));
+		// printf("[clock_gettime] Cost Time = %.3fusec\n", fCostTime);
+		
+		// libsimodel_U.inPWMs[0] = 0;
+		// libsimodel_step();
+		
+		WriteBuffer buffer{};
 		buffer.Accel_data[0] = 1;
 		buffer.Accel_data[1] = 2;
 		buffer.Accel_data[2] = 3;
 		buffer.Accel_data[3] = 4;
 		buffer.Accel_data[4] = 5;
 		buffer.Accel_data[5] = 6;
-		SensorDataBurstWrite(buffer);
+		buffer.Temp_data[0] = 7;
+		buffer.Temp_data[1] = 8;
+		buffer.Gyro_data[0] = 9;
+		buffer.Gyro_data[1] = 10;
+		buffer.Gyro_data[2] = 11;
+		buffer.Gyro_data[3] = 12;
+		buffer.Gyro_data[4] = 13;
+		buffer.Gyro_data[5] = 14;
 		
-		//
-		// fadd_U.x = 1;
-		// fadd_U.y = 2;
-		// fadd_step();
-		// printf("add output is: %f\n",fadd_Y.z);
+		// clock_gettime(CLOCK_MONOTONIC, &tBeginTime_spi);
+		SensorDataBurstWrite(buffer);
+		// RegisterWrite(0x00, 0x05);
+		// libsimodel_step();
+		// clock_gettime(CLOCK_MONOTONIC, &tEndTime_spi);
+		// double fCostTime_spi = (tEndTime_spi.tv_sec-tBeginTime_spi.tv_sec)*1000000 + (double)(tEndTime_spi.tv_nsec-tBeginTime_spi.tv_nsec)/1000;
+		// printf("[clock_gettime_spi] Cost Time = %.3fusec\n", fCostTime_spi);
 
-		//
-		libsimodel_U.inPWMs[0] = 0;
-		libsimodel_step();
+		// ReadBuffer rbuffer{};
+		// SensorDataBurstRead(rbuffer);
+		// printf("read data is %d\n", rbuffer.Accel_data[0]);
+		// uint8_t reg_data = RegisterRead(0x01);
+		// printf("data read is: %d\n", reg_data);
 
-        // 创建消息
-        std_msgs::msg::String message;
-        message.data = "forward";
-        // 日志打印
+        /* 创建消息 */ 
+        // std_msgs::msg::String message;
+        // message.data = "forward";
         // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        // 发布消息
-        command_publisher_->publish(message);
+        // command_publisher_->publish(message);
     }
 
 	template <typename T>
@@ -115,7 +146,11 @@ private:
 		transfer(cmd, cmd, sizeof(cmd));
 	}
 
-	void SensorDataBurstWrite(TransferBuffer buffer)
+	void SensorDataBurstWrite(WriteBuffer buffer)
+	{
+		transfer((uint8_t *)&buffer, (uint8_t *)&buffer, sizeof(buffer));
+	}
+	void SensorDataBurstRead(ReadBuffer buffer)
 	{
 		transfer((uint8_t *)&buffer, (uint8_t *)&buffer, sizeof(buffer));
 	}
