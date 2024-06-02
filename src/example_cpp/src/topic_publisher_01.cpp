0edda588371caf80efa7fa58028d3bab0f6a6001
kcx064 <kcx064@163.com>
2024-06-02 20:35:55 +0800

    在example_cpp中增加测试内容

--- src/example_cpp/src/topic_publisher_01.cpp
+++ src/example_cpp/src/topic_publisher_01.cpp
@@ -6,12 +6,12 @@
 
 
 extern "C"{
-	#include "fadd.h"
+	// #include "fadd.h"
 	#include "libsimodel.h"
 }
 
 
-#define spi0_device		"/dev/spidev1.0"
+#define spi1_device		"/dev/spidev1.0"
 
 class TopicPublisher01 : public rclcpp::Node, public SPI
 {
@@ -23,94 +23,84 @@ public:
 		
 		//初始化SPI，并进行测试
 		//initial spi0
-		int ret = SPI::init(spi0_device, SPIDEV_MODE3, 18000000);//SPIDEV_MODE3
+		int ret = SPI::init(spi1_device, SPIDEV_MODE3, 10000000);//SPIDEV_MODE3
 		if (ret != PX4_OK) {
 			printf("SPI::init failed (%i)", ret);
 		}
-
-		uint8_t reg_data;
-
-		RegisterWrite(0x00, 0x00);
-		reg_data = RegisterRead(0x00);
-		printf("data read is: %d\n", reg_data);
-
-		RegisterWrite(0x00, 0x05);
-		reg_data = RegisterRead(0x00);
-		printf("data read is: %d\n", reg_data);
-
-		reg_data = RegisterRead(0x01);
-		printf("data read is: %d\n", reg_data);
-
-		reg_data = RegisterRead(0x02);
-		printf("data read is: %d\n", reg_data);
-
-		reg_data = RegisterRead(0x03);
-		printf("data read is: %d\n", reg_data);
-
-		reg_data = RegisterRead(0x04);
-		printf("data read is: %d\n", reg_data);
-
-		reg_data = RegisterRead(0x05);
-		printf("data read is: %d\n", reg_data);
-
-		libsimodel_initialize();
-
+		// RegisterWrite(0x01, 0x88);
+		// RCLCPP_INFO(this->get_logger(), "write 0x01 = %x",RegisterRead(0x01));
+		icm20689reg icmreg;
+		SensorRegBurstRead(&icmreg);
+		for (int i = 0; i < 130; ++i) {
+			RCLCPP_INFO(this->get_logger(), "reg %x(%d) data read is: %x\n", i, i, icmreg.reg_data[i]);
+		}
+		// libsimodel_initialize();
+		// RegisterWrite(0x6b, 0x40);
+		RCLCPP_INFO(this->get_logger(), "write 0x6b = %x",RegisterRead(0x6b));
+		RCLCPP_INFO(this->get_logger(), "Read 0x75(117) = %x",RegisterRead(0x75));
+		
 		// 创建发布者
         command_publisher_ = this->create_publisher<std_msgs::msg::String>("command", 10);
 		// 创建定时器，xxxms为周期，定时发布
 		//milliseconds表示毫秒  microseconds表示微妙
-        timer_ = this->create_wall_timer(std::chrono::microseconds(125), std::bind(&TopicPublisher01::timer_callback, this));
+        timer_ = this->create_wall_timer(std::chrono::microseconds(1000), std::bind(&TopicPublisher01::timer_callback, this));
     }
 
 private:
 	struct timespec tBeginTime, tEndTime;
 	struct timespec tBeginTime_spi, tEndTime_spi;
+	struct icm20689reg {
+		uint8_t cmd{0x80};
+		uint8_t reg_data[130] {0,};
+	};
 	// Transfer data
-	struct WriteBuffer {
-		uint8_t cmd{0x00 | 0x00};
-		uint8_t Gyro_data[6] {0,};
-		uint8_t Temp_data[2] {0,};
-		uint8_t Accel_data[6] {0,};
-		uint8_t Mag_data[6] {0,};
-		uint8_t Pressure_data[6] {0,};
-	};
-	struct ReadBuffer {
-		uint8_t cmd{0x00 | 0x80};
-		uint8_t Gyro_data[6] {0,};
-		uint8_t Temp_data[2] {0,};
-		uint8_t Accel_data[6] {0,};
-		uint8_t Mag_data[6] {0,};
-		uint8_t Pressure_data[6] {0,};
-	};
+	// struct WriteBuffer {
+	// 	uint8_t cmd{0x00 | 0x00};
+	// 	uint8_t Gyro_data[6] {0,};
+	// 	uint8_t Temp_data[2] {0,};
+	// 	uint8_t Accel_data[6] {0,};
+	// 	uint8_t Mag_data[6] {0,};
+	// 	uint8_t Pressure_data[6] {0,};
+	// };
+	// struct ReadBuffer {
+	// 	uint8_t cmd{0x00 | 0x80};
+	// 	uint8_t Gyro_data[6] {0,};
+	// 	uint8_t Temp_data[2] {0,};
+	// 	uint8_t Accel_data[6] {0,};
+	// 	uint8_t Mag_data[6] {0,};
+	// 	uint8_t Pressure_data[6] {0,};
+	// };
 	void timer_callback()
     {
+		icm20689reg icmreg;
+		SensorRegBurstRead(&icmreg);
 		//得到当前时间，然后与之前的时间计算插值
-		clock_gettime(CLOCK_MONOTONIC, &tEndTime);
-		double fCostTime = (tEndTime.tv_sec-tBeginTime.tv_sec)*1000000 + (double)(tEndTime.tv_nsec-tBeginTime.tv_nsec)/1000;
-		memcpy(&tBeginTime,&tEndTime,sizeof(tBeginTime));
+		// clock_gettime(CLOCK_MONOTONIC, &tEndTime);
+		// double fCostTime = (tEndTime.tv_sec-tBeginTime.tv_sec)*1000000 + (double)(tEndTime.tv_nsec-tBeginTime.tv_nsec)/1000;
+		// memcpy(&tBeginTime,&tEndTime,sizeof(tBeginTime));
 		// printf("[clock_gettime] Cost Time = %.3fusec\n", fCostTime);
 		
 		// libsimodel_U.inPWMs[0] = 0;
 		// libsimodel_step();
 		
-		WriteBuffer buffer{};
-		buffer.Accel_data[0] = 1;
-		buffer.Accel_data[1] = 2;
-		buffer.Accel_data[2] = 3;
-		buffer.Accel_data[3] = 4;
-		buffer.Accel_data[4] = 5;
-		buffer.Accel_data[5] = 6;
-		buffer.Temp_data[0] = 7;
-		buffer.Temp_data[1] = 8;
-		buffer.Gyro_data[0] = 9;
-		buffer.Gyro_data[1] = 10;
-		buffer.Gyro_data[2] = 11;
-		buffer.Gyro_data[3] = 12;
-		buffer.Gyro_data[4] = 13;
-		buffer.Gyro_data[5] = 14;
+		// WriteBuffer buffer{};
+		// buffer.Accel_data[0] = 1;
+		// buffer.Accel_data[1] = 2;
+		// buffer.Accel_data[2] = 3;
+		// buffer.Accel_data[3] = 4;
+		// buffer.Accel_data[4] = 5;
+		// buffer.Accel_data[5] = 6;
+		// buffer.Temp_data[0] = 7;
+		// buffer.Temp_data[1] = 8;
+		// buffer.Gyro_data[0] = 9;
+		// buffer.Gyro_data[1] = 10;
+		// buffer.Gyro_data[2] = 11;
+		// buffer.Gyro_data[3] = 12;
+		// buffer.Gyro_data[4] = 13;
+		// buffer.Gyro_data[5] = 14;
 		
 		// clock_gettime(CLOCK_MONOTONIC, &tBeginTime_spi);
-		SensorDataBurstWrite(buffer);
+		// SensorDataBurstWrite(buffer);
 		// RegisterWrite(0x00, 0x05);
 		// libsimodel_step();
 		// clock_gettime(CLOCK_MONOTONIC, &tEndTime_spi);
@@ -146,13 +136,23 @@ private:
 		transfer(cmd, cmd, sizeof(cmd));
 	}
 
-	void SensorDataBurstWrite(WriteBuffer buffer)
+	// uint8_t SensorDataBurstWrite(BurstBuffer *buffer)
+	// {
+	// 	//设置为写命令
+	// 	buffer->cmd = 0x00;
+	// 	return transfer((uint8_t *)buffer, (uint8_t *)buffer, sizeof(BurstBuffer));
+	// }
+	// uint8_t SensorDataBurstRead(BurstBuffer *buffer)
+	// {
+	// 	//设置为读命令
+	// 	buffer->cmd = 0x80;
+	// 	return transfer((uint8_t *)buffer, (uint8_t *)buffer, sizeof(BurstBuffer));
+	// }
+	uint8_t SensorRegBurstRead(icm20689reg *buffer)
 	{
-		transfer((uint8_t *)&buffer, (uint8_t *)&buffer, sizeof(buffer));
-	}
-	void SensorDataBurstRead(ReadBuffer buffer)
-	{
-		transfer((uint8_t *)&buffer, (uint8_t *)&buffer, sizeof(buffer));
+		//设置为读命令
+		buffer->cmd = 0x80;
+		return transfer((uint8_t *)buffer, (uint8_t *)buffer, sizeof(icm20689reg));
 	}
 
     // 声名定时器指针
